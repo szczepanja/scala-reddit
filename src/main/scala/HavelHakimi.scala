1ca2c2c055aa212aebc450927728a1059ed1add3
a_szczepanik <a.szczepanik@adform.com>
2022-04-15 15:43:36 +0200

    Subtract 1 from each of the first N elements of the new sequence and continue from step 1

--- src/main/scala/HavelHakimi.scala
+++ src/main/scala/HavelHakimi.scala
@@ -2,13 +2,17 @@ import scala.annotation.tailrec
 
 object HavelHakimi {
 
+  @tailrec
   def isMeetingPossible(list: List[Int]): Boolean = list.filterNot(_ == 0).sorted.reverse match {
     case Nil => true
 
     case ls =>
       val (head, tail) = (ls.head, ls.tail)
       if (head > tail.length) false
-      else true
+      else {
+        val (ls, y) = tail.splitAt(head)
+        isMeetingPossible(ls.map(_ - 1) ::: y)
+      }
   }
 
 }
