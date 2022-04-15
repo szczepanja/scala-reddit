16b53b4e8cd4ee7de90a63728843db292261413c
a_szczepanik <a.szczepanik@adform.com>
2022-04-15 15:42:45 +0200

    Return false if head is greater than tails lenght

--- src/main/scala/HavelHakimi.scala
+++ src/main/scala/HavelHakimi.scala
@@ -4,6 +4,11 @@ object HavelHakimi {
 
   def isMeetingPossible(list: List[Int]): Boolean = list.filterNot(_ == 0).sorted.reverse match {
     case Nil => true
+
+    case ls =>
+      val (head, tail) = (ls.head, ls.tail)
+      if (head > tail.length) false
+      else true
   }
 
 }
