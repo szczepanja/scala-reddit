843c32a9e5ee408103508fc9693df4a7e5145367
a_szczepanik <a.szczepanik@adform.com>
2022-04-15 15:40:06 +0200

    Add new tests

--- src/test/scala/HavelHakimiSpec.scala
+++ src/test/scala/HavelHakimiSpec.scala
@@ -5,10 +5,24 @@ import matchers._
 
 class HavelHakimiSpec extends AnyFlatSpec with should.Matchers {
 
-  "warmup" should "given a sequence of answers, return the same set of answers with all the 0's removed" in {
-    warmup(Array(5, 3, 0, 2, 6, 2, 0, 7, 2, 5)) shouldBe Array(5, 3, 2, 6, 2, 7, 2, 5)
-    warmup(Array(4, 0, 0, 1, 3)) shouldBe Array(4, 1, 3)
-    warmup(Array(1, 2, 3)) shouldBe Array(1, 2, 3)
+  "isMeetingPossible" should "return true given a sequence of answers to the question, apply the Havel-Hakimi algorithm to determine whether or not it's possible that everyone was telling the truth" in {
+
+    isMeetingPossible(List(3, 1, 2, 3, 1, 0)) shouldBe true
+    isMeetingPossible(List(16, 9, 9, 15, 9, 7, 9, 11, 17, 11, 4, 9, 12, 14, 14, 12, 17, 0, 3, 16)) shouldBe true
+    isMeetingPossible(List(14, 10, 17, 13, 4, 8, 6, 7, 13, 13, 17, 18, 8, 17, 2, 14, 6, 4, 7, 12)) shouldBe true
+    isMeetingPossible(List(1, 1)) shouldBe true
+    isMeetingPossible(List()) shouldBe true
+  }
+
+  "isMeetingPossible" should "return false" in {
+
+    isMeetingPossible(List(4, 2, 0, 1, 5, 0)) shouldBe false
+    isMeetingPossible(List(15, 18, 6, 13, 12, 4, 4, 14, 1, 6, 18, 2, 6, 16, 0, 9, 10, 7, 12, 3)) shouldBe false
+    isMeetingPossible(List(6, 0, 10, 10, 10, 5, 8, 3, 0, 14, 16, 2, 13, 1, 2, 13, 6, 15, 5, 1)) shouldBe false
+    isMeetingPossible(List(2, 2, 0)) shouldBe false
+    isMeetingPossible(List(3, 2, 1)) shouldBe false
+    isMeetingPossible(List(1)) shouldBe false
+
   }
 
 }
